301db64230b595b2b919c26fb4877667021f16cc
rucasca <rucasca@alumni.uv.es>
2025-01-01 18:53:56 +0100

    added data load

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,16 @@
 import yaml
 import os
 import numpy as np
+import requests
+import zipfile
+
+
 
 DIR_DATA_PREPROCESSED_TRAIN = os.path.join(
     os.path.dirname(__file__), "..", "data", "preprocessed_train")
 
 
-# FUNCIONES RELATIVAS A LAS CONTRANTES
+# FUNCIONES RELATIVAS A LAS GESTION DE FICHEROS
 def load_yaml_file() -> dict:
 
     """
@@ -17,10 +21,48 @@ def load_yaml_file() -> dict:
 
     """
     
-    path =os.path.join(os.path.dirname(__file__), r"config.yml")
+    path =os.path.join(os.path.dirname(__file__),"..", r"config.yml")
 
     with open(path, 'r', encoding='utf-8', errors='ignore') as file:
             return yaml.safe_load(file)
+    
+
+
+# DESCARGA DE UN FICHERO ZIP   
+def download_zip(url:str, destination_folder:str, zip_filename:str):
+
+    """Carga de un zip de internet"""
+    os.makedirs(destination_folder, exist_ok=True)
+    zip_path = os.path.join(destination_folder, zip_filename)
+    
+    # Lanza la petición
+    response = requests.get(url)
+    
+    # Si devuelve 200 (exito), guarda en memoria en formato .zip
+    if response.status_code == 200:
+        with open(zip_path, 'wb') as f:
+            print("Fichero encontrado")
+            f.write(response.content)
+
+        print("fichero descargado")
+
+        # Hace unzip del resultado
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            print("fichero descompruimido")
+
+            zip_ref.extractall(destination_folder)
+
+        os.remove(zip_path)
+
+        print(f"ZIP file downloaded to: {zip_path}")
+    else:
+        print(f"fallo en la descarga del fichero: {response.status_code}")
+
+    return 
+
+
+
+
     
 
 
