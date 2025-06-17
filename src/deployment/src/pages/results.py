434f309f01a2b6919ffb6fe22a696aed2e637c57
rucasca <rucasca@alumni.uv.es>
2025-06-17 23:02:03 +0200

    changes in model pipeline & others

--- src/deployment/src/pages/results.py
+++ src/deployment/src/pages/results.py
@@ -3,6 +3,8 @@ from dash import html
 
 dash.register_page(__name__, path  = "/results", name = "Ejemplos")
 
+path = r"C:\Users\ruben\Desktop\code_tfm\src\deployment\src\outputs"
+
 layout = html.Div([
     html.H1('This is our Archive page'),
     html.Div('This is our Archive page content.'),
